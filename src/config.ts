4b597e6f1b4b2476ea86f89d3a4c7cdbe5c29d51
docodepractices <tridang270395@gmail.com>
2023-06-24 15:44:02 +0700

    add deployment files

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '31hb2yu658'
+const apiId = 'qieq8psd9e'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
@@ -7,5 +7,5 @@ export const authConfig = {
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'test-quotes-2703.us.auth0.com',            // Auth0 domain
   clientId: 'omdcRUYaAUkcUG1MtPwk5v2yrabHY5fC',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://a1e35dd0b168841ec857ded820a677b6-1911967983.us-east-1.elb.amazonaws.com:8080/callback'
 }
